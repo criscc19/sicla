20a3d4975ca2f05ce3a955e06964a628880c7b02
Christopher <criscc19@hotmail.com>
2019-09-12 14:05:40 -0600

    Create plantilla_factura.php

---
+++ plantilla_factura.php
@@ -0,0 +1,137 @@
+<div class="tabBar">
+<div class="arearef heightref valignmiddle" width="100%">
+<!-- Start banner content -->
+<div style="vertical-align: middle">
+<div class="inline-block floatleft valignmiddle refid refidpadding">(PROV26) <div class="refidno">Ref. cliente 
+: Factura desde seminario<br>
+  Tercero : KARLA REBECA ARAYA ALVARADO (Otras facturas)</div><div class="refidno">
+  <!-- BEGIN part to show address block --><div style="clear: both;"></div><div style="clear: both;"></div>
+  <!-- END Part to show address block --></div></div></div>
+  <!-- End banner content --></div>
+  <div class="underrefbanner clearboth"></div>
+  <div class="fichecenter"><div class="fichehalfleft">
+  <div class="underbanner clearboth"></div>
+  <table class="border" width="100%"><tbody>
+  <tr><td class="titlefield">Tipo</td>
+<td>Factura estándar</td>
+</tr>
+<!-- Discounts --><tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td>Fecha facturación</td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>12/09/2019</td>
+</tr>
+<tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td>Condiciones de pago</td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>Acuse de recibo</td>
+</tr>
+<tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td>Fecha límite de pago</td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>13/09/2019</td>
+</tr>
+<tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td>Forma de pago</td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>Pago TIP</td>
+</tr>
+<tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td><label for="multicurrency_code">Divisa</label></td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>CRC - Costa Rica Colon</td>
+</tr>
+<tr><td><table class="nobordernopadding" width="100%"><tbody><tr><td><label for="multicurrency_tx">Tasa de conversión de moneda</label></td>
+</tr>
+</tbody></table></td>
+<td>1</td>
+</tr>
+<tr><td class="nowrap"><table width="100%" class="nobordernopadding"><tbody><tr><td class="nowrap">Cuenta bancaria</td>
+<td></td>
+<td align="right"></td>
+</tr>
+</tbody></table></td>
+<td>&nbsp;</td>
+</tr>
+<!-- BEGIN PHP TEMPLATE extrafields_view.tpl.php -->
+<!-- END PHP TEMPLATE extrafields_view.tpl.php -->
+</tbody>
+</table>
+</div><div class="fichehalfright"><div class="ficheaddleft"><div class="underbanner clearboth"></div>
+<table class="border centpercent"><tbody><tr><td class="titlefieldmiddle">Base imponible</td>
+<td class="nowrap amountcard">4.092,51 ₡</td>
+</tr>
+<tr><td>Importe IVA</td>
+<td colspan="3" class="nowrap amountcard">532,02 ₡</td>
+</tr>
+<tr><td>Importe total</td>
+<td class="nowrap amountcard">4.624,53 ₡</td>
+</tr>
+</tbody></table><div class="div-table-responsive-no-min"><table class="noborder paymenttable" width="100%"><tbody><tr class="liste_titre"><td class="liste_titre">Pagos</td>
+<td class="liste_titre">Fecha</td>
+<td class="liste_titre">Tipo</td>
+<td class="liste_titre" align="right">Cuenta bancaria</td>
+<td class="liste_titre" align="right">Importe</td>
+</tr>
+<tr><td colspan="4" align="right">Ya pagado (excluidos los abonos y anticipos) :</td>
+<td align="right">0,00</td>
+</tr>
+<tr><td colspan="4" align="right">Facturado :</td>
+<td align="right">4.624,53</td>
+</tr>
+<tr><td colspan="4" align="right">Resta por pagar :</td>
+<td align="right" class="amountremaintopay">4.624,53</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+<div class="clearboth"></div
+><br>
+
+
+<!-- END PHP TEMPLATE AJAXROW.TPL.PHP --><div class="div-table-responsive-no-min"><table id="tablelines" class="noborder noshadow" width="100%"><thead>
+<tr class="liste_titre nodrag nodrop"><td class="linecoldescription">Descripción</td>
+<td class="linecolvat" align="right" width="80">IVA</td>
+<td class="linecoluht" align="right" width="80">P.U.</td>
+<td class="linecolqty" align="right">Cant.</td>
+<td class="linecoldiscount" align="right">Dto.</td>
+<td class="linecolht" align="right">Importe</td>
+<td class="linecoledit"></td>
+<td class="linecoldelete" width="10"></td>
+<td class="linecolmove" width="10"></td>
+</tr>
+
+</thead>
+<tbody>
+<!-- BEGIN PHP TEMPLATE objectline_view.tpl.php -->
+<tr id="row-11" class="drag drop oddeven" data-element="facturedet" data-id="11" data-qty="100" data-product_type="0">
+<td class="linecoldescription minwidth300imp">SER-000001 - COPIA CARTA BN 1L	</td>
+<td align="right" class="linecolvat nowrap">13%</td>
+<td align="right" class="linecoluht nowrap">13,27</td>
+<td align="right" class="linecolqty nowrap">	100	</td>
+<td class="linecoldiscount">&nbsp;</td>
+<td align="right" class="linecolht nowrap">1.327,00</td>
+<td class="linecoledit" align="center">	
+</td>
+<td class="linecoldelete" align="center"></td>
+<td align="center" class="linecolmove tdlineupdown">
+ </td>  	
+</tr>
+
+</tbody>
+
+<tbody>
+<tr id="trlinefordates">
+<td colspan="9">
+</tbody>
+</table>
+</div>
+
+</div>
